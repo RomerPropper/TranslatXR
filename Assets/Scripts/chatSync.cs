29ddd33e5f47f6ff269ba20a7d226d9e3a8d2691
dndawson <dawson@dndconsolidated.com>
2024-03-28 18:04:48 -0500

    Updated chatSync with comments

--- Assets/Scripts/chatSync.cs
+++ Assets/Scripts/chatSync.cs
@@ -10,7 +10,7 @@ using UnityEngine.Windows;
 
 public class chatSync : RealtimeComponent<chatSyncModel>
 {
-
+    //This will allow classes to "subscribe" to any new Messages so they are notified.
     public delegate void MessageChangedEventHandler(Message newMessage);
     public event MessageChangedEventHandler MessageChanged;
 
@@ -37,7 +37,9 @@ public class chatSync : RealtimeComponent<chatSyncModel>
         NotifyObservers(Message.parseFromJson(model.jsonMessage));
     }
 
-    //Use this function anytime you want to 
+    //Use this function anytime you want to send a message across the network.
+    //This will update the model with the message details which will sync to all other clients.
+    //This needs to be called from the game master after a transcription and sentiment analytsis has been done.
     public void SendMessage(Message newMessage) {
         model.jsonMessage = newMessage.convertToJson();
     }
