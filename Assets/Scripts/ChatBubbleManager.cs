a2e6a719a1f9f7a8d89ae681de8d29d1337a64ca
earmando29 <earmando29@gmail.com>
2024-04-10 13:42:40 -0500

    feat: adjusted for use with realtime
    Untested thus far.

--- Assets/Scripts/ChatBubbleManager.cs
+++ Assets/Scripts/ChatBubbleManager.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 
 public class ChatBubbleManager : MonoBehaviour
 {
-    public GameObject chatBubblePrefab; // Assign your chat bubble prefab here
+    public GameObject chatBubblePrefab; 
+    public Realtime realtime; 
 
     // Call this method when you need to create a chat bubble for a user
+    private void Awake()
+    {
+        // Find the Realtime component in the scene
+        realtime = FindObjectOfType<Realtime>();
+    }
     public void CreateChatBubbleForUser(RealtimeAvatar userAvatar)
     {
         GameObject chatBubble = Instantiate(chatBubblePrefab, userAvatar.head.position, Quaternion.identity);
         chatBubble.GetComponent<ChatBubble>().SetTarget(userAvatar.head);
+    }
+
+    public void TryCreateChatBubbleForUser(RealtimeAvatar userAvatar)
+    {
+        // Check if we're connected to a room before creating a chat bubble
+        if (realtime.connected)
+        {
+            GameObject chatBubble = Instantiate(chatBubblePrefab, userAvatar.head.position, Quaternion.identity);
+            chatBubble.GetComponent<ChatBubble>().SetTarget(userAvatar.head);
+        }
     }
 }
