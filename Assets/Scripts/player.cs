ef1e5e0d8a877921a7b76539248821a4810531db
Mason7579 <yzhan308@cougarnet.uh.edu>
2024-04-15 00:19:49 -0500

    Feat: get the head Location

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,269 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Normal.Utility;
+
+namespace Normal.Realtime
+{
+    [DefaultExecutionOrder(player.ExecutionOrder)] // Make sure our Update() runs before the default to so that the avatar positions are as up to date as possible when everyone else's Update() runs.
+    public class player : RealtimeComponent<playerModel>
+    {
+        public const int ExecutionOrder = -90;
+        public delegate void RightHandPositionChanged(Transform rightHandTransform);
+        // Local Player
+        [Serializable]
+        public class LocalPlayer
+        {
+            public Transform root;
+            public Transform head;
+            public Transform leftHand;
+            public Transform rightHand;
+        }
+
+        public LocalPlayer localPlayer
+        {
+            get => _localPlayer;
+            set => SetLocalPlayer(value);
+        }
+
+#pragma warning disable 0649 // Disable variable is never assigned to warning.
+        private LocalPlayer _localPlayer;
+#pragma warning restore 0649
+
+        // Device Type
+        public enum DeviceType : uint
+        {
+            Unknown = 0,
+            OpenVR = 1,
+            Oculus = 2,
+        }
+
+        /// <summary>
+        /// The XR device type of the client that owns this avatar. See RealtimeAvatar#DeviceType for values.
+        /// </summary>
+        public DeviceType deviceType
+        {
+            get => model.deviceType;
+            set => model.deviceType = value;
+        }
+
+        /// <summary>
+        /// The XRDevice.model of the client that owns this avatar.
+        /// </summary>
+        public string deviceModel
+        {
+            get => model.deviceModel;
+            set => model.deviceModel = value;
+        }
+
+        // Prefab
+        public Transform head => _head;
+        public Transform leftHand => _leftHand;
+        public Transform rightHand => _rightHand;
+
+#pragma warning disable 0649 // Disable variable is never assigned to warning.
+        [SerializeField] private Transform _head;
+        [SerializeField] private Transform _leftHand;
+        [SerializeField] private Transform _rightHand;
+#pragma warning restore 0649
+
+        private plyerManagement _realtimeAvatarManager;
+
+        private static List<XRNodeState> _nodeStates = new List<XRNodeState>();
+
+        void Start()
+        {
+            // Register with RealtimeAvatarManager
+            try
+            {
+                _realtimeAvatarManager = realtime.GetComponent<plyerManagement>();
+                _realtimeAvatarManager._RegisterAvatar(realtimeView.ownerIDSelf, this);
+            }
+            catch (NullReferenceException)
+            {
+                Debug.LogError("RealtimeAvatar failed to register with RealtimeAvatarManager component. Was this avatar prefab instantiated by RealtimeAvatarManager?");
+            }
+        }
+
+        void OnDestroy()
+        {
+            // Unregister with RealtimeAvatarManager
+            if (_realtimeAvatarManager != null)
+                _realtimeAvatarManager._UnregisterAvatar(this);
+
+            // Unregister for events
+            localPlayer = null;
+        }
+
+        void FixedUpdate()
+        {
+            UpdateAvatarTransformsForLocalPlayer();
+        }
+
+        void Update()
+        {
+            UpdateAvatarTransformsForLocalPlayer();
+        }
+
+        void LateUpdate()
+        {
+            UpdateAvatarTransformsForLocalPlayer();
+        }
+
+        protected override void OnRealtimeModelReplaced(playerModel previousModel, playerModel currentModel)
+        {
+            if (previousModel != null)
+            {
+                previousModel.headActiveDidChange -= ActiveStateChanged;
+                previousModel.leftHandActiveDidChange -= ActiveStateChanged;
+                previousModel.rightHandActiveDidChange -= ActiveStateChanged;
+            }
+
+            if (currentModel != null)
+            {
+                currentModel.headActiveDidChange += ActiveStateChanged;
+                currentModel.leftHandActiveDidChange += ActiveStateChanged;
+                currentModel.rightHandActiveDidChange += ActiveStateChanged;
+            }
+        }
+
+        void SetLocalPlayer(LocalPlayer localPlayer)
+        {
+            if (localPlayer == _localPlayer)
+                return;
+
+            _localPlayer = localPlayer;
+
+            if (_localPlayer != null)
+            {
+                RealtimeTransform rootRealtimeTransform = GetComponent<RealtimeTransform>();
+                RealtimeTransform headRealtimeTransform = _head != null ? _head.GetComponent<RealtimeTransform>() : null;
+                RealtimeTransform leftHandRealtimeTransform = _leftHand != null ? _leftHand.GetComponent<RealtimeTransform>() : null;
+                RealtimeTransform rightHandRealtimeTransform = _rightHand != null ? _rightHand.GetComponent<RealtimeTransform>() : null;
+                if (rootRealtimeTransform != null) rootRealtimeTransform.RequestOwnership();
+                if (headRealtimeTransform != null) headRealtimeTransform.RequestOwnership();
+                if (leftHandRealtimeTransform != null) leftHandRealtimeTransform.RequestOwnership();
+                if (rightHandRealtimeTransform != null) rightHandRealtimeTransform.RequestOwnership();
+            }
+        }
+
+        void ActiveStateChanged(playerModel model, bool nodeIsActive)
+        {
+            // Leave the head active so RealtimeAvatarVoice runs even when the head isn't tracking.
+            if (_leftHand != null) _leftHand.gameObject.SetActive(model.leftHandActive);
+            if (_rightHand != null) _rightHand.gameObject.SetActive(model.rightHandActive);
+        }
+
+        void UpdateAvatarTransformsForLocalPlayer()
+        {
+            // Make sure this avatar is a local player
+            if (_localPlayer == null)
+                return;
+
+            // Flags to fetch XRNode position/rotation state
+            bool updateHeadWithXRNode = false;
+            bool updateLeftHandWithXRNode = false;
+            bool updateRightHandWithXRNode = false;
+
+            // Root
+            if (_localPlayer.root != null)
+            {
+                transform.position = _localPlayer.root.position;
+                transform.rotation = _localPlayer.root.rotation;
+                transform.localScale = _localPlayer.root.localScale;
+            }
+
+            // Head
+            if (_localPlayer.head != null)
+            {
+                model.headActive = _localPlayer.head.gameObject.activeSelf;
+
+                _head.position = _localPlayer.head.position;
+                _head.rotation = _localPlayer.head.rotation;
+            }
+            else
+            {
+                updateHeadWithXRNode = true;
+            }
+
+            // Left Hand
+            if (_leftHand != null)
+            {
+                if (_localPlayer.leftHand != null)
+                {
+                    model.leftHandActive = _localPlayer.leftHand.gameObject.activeSelf;
+
+                    _leftHand.position = _localPlayer.leftHand.position;
+                    _leftHand.rotation = _localPlayer.leftHand.rotation;
+                }
+                else
+                {
+                    updateLeftHandWithXRNode = true;
+                }
+            }
+
+            // Right Hand
+            if (_rightHand != null)
+            {
+                if (_localPlayer.rightHand != null)
+                {
+                    model.rightHandActive = _localPlayer.rightHand.gameObject.activeSelf;
+
+                    _rightHand.position = _localPlayer.rightHand.position;
+                    _rightHand.rotation = _localPlayer.rightHand.rotation;
+                }
+                else
+                {
+                    updateRightHandWithXRNode = true;
+                }
+            }
+
+            // Update head/hands using XRNode APIs if needed
+            if (updateHeadWithXRNode || updateLeftHandWithXRNode || updateRightHandWithXRNode)
+            {
+                InputTracking.GetNodeStates(_nodeStates); // the list is cleared by GetNodeStates
+
+                bool headActive = false;
+                bool leftHandActive = false;
+                bool rightHandActive = false;
+
+                foreach (XRNodeState nodeState in _nodeStates)
+                {
+                    if (nodeState.nodeType == XRNode.Head && updateHeadWithXRNode)
+                    {
+                        headActive = nodeState.tracked;
+                        UpdateTransformWithNodeState(_head, nodeState);
+                    }
+                    else if (nodeState.nodeType == XRNode.LeftHand && updateLeftHandWithXRNode)
+                    {
+                        leftHandActive = nodeState.tracked;
+                        UpdateTransformWithNodeState(_leftHand, nodeState);
+                    }
+                    else if (nodeState.nodeType == XRNode.RightHand && updateRightHandWithXRNode)
+                    {
+                        rightHandActive = nodeState.tracked;
+                        UpdateTransformWithNodeState(_rightHand, nodeState);
+                    }
+                }
+
+                if (updateHeadWithXRNode) model.headActive = headActive;
+                if (updateLeftHandWithXRNode) model.leftHandActive = leftHandActive;
+                if (updateRightHandWithXRNode) model.rightHandActive = rightHandActive;
+            }
+        }
+
+        private static void UpdateTransformWithNodeState(Transform transform, XRNodeState state)
+        {
+            if (state.TryGetPosition(out Vector3 position))
+            {
+                transform.localPosition = position;
+            }
+
+            if (state.TryGetRotation(out Quaternion rotation))
+            {
+                transform.localRotation = rotation;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/playerModel.cs
@@ -0,0 +1,216 @@
+using System;
+using Normal.Realtime;
+using Normal.Realtime.Serialization;
+
+namespace Normal.Realtime
+{
+    [RealtimeModel]
+    public partial class playerModel
+    {
+        // Property 1 bitflag transitioned to 4/5/6 bools
+        [RealtimeProperty(2, true)] private player.DeviceType _deviceType;
+        [RealtimeProperty(3, true)] private string _deviceModel;
+        [RealtimeProperty(4, true, true)] private bool _headActive;
+        [RealtimeProperty(5, true, true)] private bool _leftHandActive;
+        [RealtimeProperty(6, true, true)] private bool _rightHandActive;
+    }
+}
+
+#if !UNITY_2021_1_OR_NEWER
+namespace Normal.Realtime {
+    public partial class playerModel : RealtimeModel {
+        public Normal.Realtime.player.DeviceType deviceType {
+            get {
+                return (Normal.Realtime.player.DeviceType)_deviceTypeProperty.value;
+            }
+            set {
+                if (_deviceTypeProperty.value == (uint)value) return;
+                _deviceTypeProperty.value = (uint)value;
+                InvalidateReliableLength();
+            }
+        }
+
+        public string deviceModel {
+            get {
+                return _deviceModelProperty.value;
+            }
+            set {
+                if (_deviceModelProperty.value == value) return;
+                _deviceModelProperty.value = value;
+                InvalidateReliableLength();
+            }
+        }
+
+        public bool headActive {
+            get {
+                return _headActiveProperty.value;
+            }
+            set {
+                if (_headActiveProperty.value == value) return;
+                _headActiveProperty.value = value;
+                InvalidateReliableLength();
+                FireHeadActiveDidChange(value);
+            }
+        }
+
+        public bool leftHandActive {
+            get {
+                return _leftHandActiveProperty.value;
+            }
+            set {
+                if (_leftHandActiveProperty.value == value) return;
+                _leftHandActiveProperty.value = value;
+                InvalidateReliableLength();
+                FireLeftHandActiveDidChange(value);
+            }
+        }
+
+        public bool rightHandActive {
+            get {
+                return _rightHandActiveProperty.value;
+            }
+            set {
+                if (_rightHandActiveProperty.value == value) return;
+                _rightHandActiveProperty.value = value;
+                InvalidateReliableLength();
+                FireRightHandActiveDidChange(value);
+            }
+        }
+
+        public delegate void PropertyChangedHandler<in T>(playerModel model, T value);
+        public event PropertyChangedHandler<bool> headActiveDidChange;
+        public event PropertyChangedHandler<bool> leftHandActiveDidChange;
+        public event PropertyChangedHandler<bool> rightHandActiveDidChange;
+
+        public enum PropertyID : uint {
+            DeviceType = 2,
+            DeviceModel = 3,
+            HeadActive = 4,
+            LeftHandActive = 5,
+            RightHandActive = 6,
+        }
+
+#region Properties
+
+        private ReliableProperty<uint> _deviceTypeProperty;
+
+        private ReliableProperty<string> _deviceModelProperty;
+
+        private ReliableProperty<bool> _headActiveProperty;
+
+        private ReliableProperty<bool> _leftHandActiveProperty;
+
+        private ReliableProperty<bool> _rightHandActiveProperty;
+
+#endregion
+
+        public playerModel() : base(null) {
+            _deviceTypeProperty = new ReliableProperty<uint>(2, (uint)_deviceType);
+            _deviceModelProperty = new ReliableProperty<string>(3, _deviceModel);
+            _headActiveProperty = new ReliableProperty<bool>(4, _headActive);
+            _leftHandActiveProperty = new ReliableProperty<bool>(5, _leftHandActive);
+            _rightHandActiveProperty = new ReliableProperty<bool>(6, _rightHandActive);
+        }
+
+        protected override void OnParentReplaced(RealtimeModel previousParent, RealtimeModel currentParent) {
+            _deviceTypeProperty.UnsubscribeCallback();
+            _deviceModelProperty.UnsubscribeCallback();
+            _headActiveProperty.UnsubscribeCallback();
+            _leftHandActiveProperty.UnsubscribeCallback();
+            _rightHandActiveProperty.UnsubscribeCallback();
+        }
+
+        private void FireHeadActiveDidChange(bool value) {
+            try {
+                headActiveDidChange?.Invoke(this, value);
+            } catch (System.Exception exception) {
+                UnityEngine.Debug.LogException(exception);
+            }
+        }
+
+        private void FireLeftHandActiveDidChange(bool value) {
+            try {
+                leftHandActiveDidChange?.Invoke(this, value);
+            } catch (System.Exception exception) {
+                UnityEngine.Debug.LogException(exception);
+            }
+        }
+
+        private void FireRightHandActiveDidChange(bool value) {
+            try {
+                rightHandActiveDidChange?.Invoke(this, value);
+            } catch (System.Exception exception) {
+                UnityEngine.Debug.LogException(exception);
+            }
+        }
+
+        protected override int WriteLength(StreamContext context) {
+            var length = 0;
+            length += _deviceTypeProperty.WriteLength(context);
+            length += _deviceModelProperty.WriteLength(context);
+            length += _headActiveProperty.WriteLength(context);
+            length += _leftHandActiveProperty.WriteLength(context);
+            length += _rightHandActiveProperty.WriteLength(context);
+            return length;
+        }
+
+        protected override void Write(WriteStream stream, StreamContext context) {
+            var writes = false;
+            writes |= _deviceTypeProperty.Write(stream, context);
+            writes |= _deviceModelProperty.Write(stream, context);
+            writes |= _headActiveProperty.Write(stream, context);
+            writes |= _leftHandActiveProperty.Write(stream, context);
+            writes |= _rightHandActiveProperty.Write(stream, context);
+            if (writes) InvalidateContextLength(context);
+        }
+
+        protected override void Read(ReadStream stream, StreamContext context) {
+            var anyPropertiesChanged = false;
+            while (stream.ReadNextPropertyID(out uint propertyID)) {
+                var changed = false;
+                switch (propertyID) {
+                    case (uint)PropertyID.DeviceType: {
+                        changed = _deviceTypeProperty.Read(stream, context);
+                        break;
+                    }
+                    case (uint)PropertyID.DeviceModel: {
+                        changed = _deviceModelProperty.Read(stream, context);
+                        break;
+                    }
+                    case (uint)PropertyID.HeadActive: {
+                        changed = _headActiveProperty.Read(stream, context);
+                        if (changed) FireHeadActiveDidChange(headActive);
+                        break;
+                    }
+                    case (uint)PropertyID.LeftHandActive: {
+                        changed = _leftHandActiveProperty.Read(stream, context);
+                        if (changed) FireLeftHandActiveDidChange(leftHandActive);
+                        break;
+                    }
+                    case (uint)PropertyID.RightHandActive: {
+                        changed = _rightHandActiveProperty.Read(stream, context);
+                        if (changed) FireRightHandActiveDidChange(rightHandActive);
+                        break;
+                    }
+                    default: {
+                        stream.SkipProperty();
+                        break;
+                    }
+                }
+                anyPropertiesChanged |= changed;
+            }
+            if (anyPropertiesChanged) {
+                UpdateBackingFields();
+            }
+        }
+
+        private void UpdateBackingFields() {
+            _deviceType = deviceType;
+            _deviceModel = deviceModel;
+            _headActive = headActive;
+            _leftHandActive = leftHandActive;
+            _rightHandActive = rightHandActive;
+        }
+    }
+}
+#endif
