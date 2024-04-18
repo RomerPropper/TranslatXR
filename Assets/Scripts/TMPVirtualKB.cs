33783f10551032418eea0582f9b64e2043986337
earmando29 <earmando29@gmail.com>
2024-04-18 11:48:05 -0500

    feat: virtual kb update

--- Assets/Scripts/TMPVirtualKB.cs
+++ Assets/Scripts/TMPVirtualKB.cs
@@ -9,36 +9,32 @@ public class TextMeshProVirtualKeyboardInputSource : MonoBehaviour
     [SerializeField]
     private TMP_InputField inputField;
 
-    private OVRVirtualKeyboard ovrVirtualKeyboard; // Reference to the OVRVirtualKeyboard component
-
     void Start()
     {
-        ovrVirtualKeyboard = virtualKeyboard.GetComponent<OVRVirtualKeyboard>();
-
         inputField.onSelect.AddListener(OnInputFieldSelect);
         // inputField.onDeselect.AddListener(OnInputFieldDeselect);
         inputField.onValueChanged.AddListener(OnInputFieldValueChange);
-        
-        ovrVirtualKeyboard.CommitTextEvent.AddListener(OnCommitText); // Using AddListener for UnityEvent
-        ovrVirtualKeyboard.BackspaceEvent.AddListener(OnBackspace);
-        ovrVirtualKeyboard.KeyboardHiddenEvent.AddListener(OnKeyboardHidden);
+
+        virtualKeyboard.CommitTextEvent.AddListener(OnCommitText);
+        virtualKeyboard.BackspaceEvent.AddListener(OnBackspace);
+        virtualKeyboard.KeyboardHiddenEvent.AddListener(OnKeyboardHidden);
     }
 
     private void OnInputFieldValueChange(string arg0)
     {
-        ovrVirtualKeyboard.ChangeTextContext(arg0);
+        virtualKeyboard.ChangeTextContext(arg0);
     }
 
     private void OnInputFieldSelect(string arg0)
     {
-        ovrVirtualKeyboard.ChangeTextContext(arg0);
+        virtualKeyboard.ChangeTextContext(arg0);
         virtualKeyboard.gameObject.SetActive(true);
     }
 
-    // private void OnInputFieldDeselect(string arg0)
-    // {
-    //     virtualKeyboard.gameObject.SetActive(false);
-    // }
+    private void OnInputFieldDeselect(string arg0)
+    {
+        virtualKeyboard.gameObject.SetActive(false);
+    }
 
     private void OnKeyboardHidden()
     {
@@ -46,12 +42,15 @@ public class TextMeshProVirtualKeyboardInputSource : MonoBehaviour
     }
 
     private void OnCommitText(string arg0)
-    {
+{
+    if (arg0 == "\n") {
         virtualKeyboard.gameObject.SetActive(false);
         inputField.DeactivateInputField();
+    } else {
         inputField.text += arg0;
         inputField.MoveTextEnd(false);
     }
+}
 
     private void OnBackspace()
     {
@@ -61,4 +60,10 @@ public class TextMeshProVirtualKeyboardInputSource : MonoBehaviour
             inputField.MoveTextEnd(false);
         }
     }
+
+    public void HideKeyboard()
+{
+    virtualKeyboard.gameObject.SetActive(false);
+    inputField.DeactivateInputField();
+}
 }
