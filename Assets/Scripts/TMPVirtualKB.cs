4f2d1e047c0ec059ea3dacdee4ab697aea661bff
earmando29 <earmando29@gmail.com>
2024-04-17 22:02:52 -0500

    feat: works for appearing
    Has bugs regarding using the keyboard
    we still need a good way to position the keyboard for the user

--- Assets/Scripts/TMPVirtualKB.cs
+++ Assets/Scripts/TMPVirtualKB.cs
@@ -9,66 +9,56 @@ public class TextMeshProVirtualKeyboardInputSource : MonoBehaviour
     [SerializeField]
     private TMP_InputField inputField;
 
+    private OVRVirtualKeyboard ovrVirtualKeyboard; // Reference to the OVRVirtualKeyboard component
+
     void Start()
     {
+        ovrVirtualKeyboard = virtualKeyboard.GetComponent<OVRVirtualKeyboard>();
+
         inputField.onSelect.AddListener(OnInputFieldSelect);
+        // inputField.onDeselect.AddListener(OnInputFieldDeselect);
         inputField.onValueChanged.AddListener(OnInputFieldValueChange);
-        virtualKeyboard.CommitText += OnCommitText;
-        virtualKeyboard.Backspace += OnBackspace;
-        virtualKeyboard.KeyboardHidden += OnKeyboardHidden;
+        
+        ovrVirtualKeyboard.CommitTextEvent.AddListener(OnCommitText); // Using AddListener for UnityEvent
+        ovrVirtualKeyboard.BackspaceEvent.AddListener(OnBackspace);
+        ovrVirtualKeyboard.KeyboardHiddenEvent.AddListener(OnKeyboardHidden);
     }
 
     private void OnInputFieldValueChange(string arg0)
     {
-        virtualKeyboard.ChangeTextContext(arg0);
+        ovrVirtualKeyboard.ChangeTextContext(arg0);
     }
 
     private void OnInputFieldSelect(string arg0)
     {
-        virtualKeyboard.ChangeTextContext(arg0);
+        ovrVirtualKeyboard.ChangeTextContext(arg0);
         virtualKeyboard.gameObject.SetActive(true);
     }
 
+    // private void OnInputFieldDeselect(string arg0)
+    // {
+    //     virtualKeyboard.gameObject.SetActive(false);
+    // }
+
     private void OnKeyboardHidden()
     {
-        if (!inputField.isFocused)
-        {
-            return;
-        }
-        // if the user hides the keyboard
         inputField.DeactivateInputField();
     }
 
     private void OnCommitText(string arg0)
     {
-        if (!inputField.isFocused)
-        {
-            return;
-        }
-        inputField.onValueChanged.RemoveListener(OnInputFieldValueChange);
-        if (arg0 == "\n" && !inputField.multiLine)
-        {
-            inputField.OnSubmit(null);
-        }
-        inputField.SetTextWithoutNotify(inputField.text + arg0);
+        virtualKeyboard.gameObject.SetActive(false);
+        inputField.DeactivateInputField();
         inputField.text += arg0;
         inputField.MoveTextEnd(false);
-        inputField.onValueChanged.AddListener(OnInputFieldValueChange);
-        inputField.DeactivateInputField();
     }
 
     private void OnBackspace()
     {
-        if (!inputField.isFocused)
-        {
-            return;
-        }
         if (inputField.text.Length > 0)
         {
-            inputField.onValueChanged.RemoveListener(OnInputFieldValueChange);
             inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
             inputField.MoveTextEnd(false);
-            inputField.onValueChanged.AddListener(OnInputFieldValueChange);
         }
     }
 }
